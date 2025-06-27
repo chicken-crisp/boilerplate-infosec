4593e9524cf5aabad222fb583e722c4fd2481ae6
chicken-crisp <nfuuma0@gmail.com>
2025-06-27 19:51:08 +0000

    edited more

--- myApp.js
+++ myApp.js
@@ -2,9 +2,6 @@ const express = require('express');
 const app = express();
 
 
-const helmet = require('helmet')
-helmet.hidePoweredBy();
-
 
 
 
@@ -62,3 +59,5 @@ let port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Your app is listening on port ${port}`);
 });
+
+app.use(helmet.hidePoweredBy())
