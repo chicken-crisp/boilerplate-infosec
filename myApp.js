f403ae240cd09ada04a1ee4e998c3c811ea4fe80
chicken-crisp <nfuuma0@gmail.com>
2025-06-27 19:34:40 +0000

    hidden poweredby

--- myApp.js
+++ myApp.js
@@ -49,12 +49,14 @@ const app = express();
 
 module.exports = app;
 const api = require('./server.js');
+const helmet = require('helmet');
 app.use(express.static('public'));
 app.disable('strict-transport-security');
 app.use('/_api', api);
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
+app.use(helmet.hidePoweredBy());
 let port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Your app is listening on port ${port}`);
