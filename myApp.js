668f5b679f87cc6c3f1b4ac8a486961ac12066ac
chicken-crisp <nfuuma0@gmail.com>
2025-06-28 17:13:53 +0000

    edit xss filter

--- myApp.js
+++ myApp.js
@@ -46,18 +46,23 @@ const app = express();
 
 
 
+
 module.exports = app;
 const api = require('./server.js');
 const helmet = require('helmet');
 app.use(express.static('public'));
 app.disable('strict-transport-security');
+app.use(helmet.hidePoweredBy())
+app.use(helmet.frameguard(
+  {action:'deny'}
+));
+app.use(helmet.xssFilter());
 app.use('/_api', api);
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
+
 let port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Your app is listening on port ${port}`);
 });
-
-app.use(helmet.hidePoweredBy())
