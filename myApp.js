b35498fb1c462e3909c0af354b190bba0372c6a4
chicken-crisp <nfuuma0@gmail.com>
2025-06-28 17:15:13 +0000

    edited noSniff

--- myApp.js
+++ myApp.js
@@ -57,6 +57,7 @@ app.use(helmet.frameguard(
   {action:'deny'}
 ));
 app.use(helmet.xssFilter());
+app.use(helmet.noSniff())
 app.use('/_api', api);
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
